738400caadd085bdc9cd113872a2b8054ccafe28
iaroslav.sviatkin <$viatk1N2013>
2019-03-12 13:25:19 +0200

    commit2

--- src/test/java/com/it/MyTest.java
+++ src/test/java/com/it/MyTest.java
@@ -1,10 +1,18 @@
 package com.it;
 
+import org.junit.Assert;
 import org.junit.Test;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class MyTest {
     @Test
     public void test1() {
+        System.setProperty("webdriver.chrome.driver", "drivers\\chromedriver.exe");
+        ChromeDriver driver = new ChromeDriver();
+        driver.get("https://mvnrepository.com");
+        String url = driver.getCurrentUrl();
+        Assert.assertEquals("https://mvnrepository.com/",url);
+        driver.quit();
 
     }
 }
